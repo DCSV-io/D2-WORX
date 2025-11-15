861c31405e33878e7681d0fe0ba0b95422805f7d
Tr-st-n <91633584+Tr-st-n@users.noreply.github.com>
2025-11-15 06:07:03 -0700

    feature(caching): remove old service classes, update extensions

--- backends/Contracts/DistributedCache.Redis/Extensions.cs
+++ backends/Contracts/DistributedCache.Redis/Extensions.cs
@@ -4,41 +4,51 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+// ReSharper disable AccessToStaticMemberViaDerivedType
 namespace D2.Contracts.DistributedCache.Redis;
 
+using D2.Contracts.DistributedCache.Redis.Handlers;
 using D2.Contracts.Interfaces.DistributedCacheService;
 using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
 
 /// <summary>
-/// Provides extension methods related to the <see cref="RedisDistributedCacheService"/> and
-/// <see cref="IServiceCollection"/>.
+/// Extension methods for adding Redis distributed caching services.
 /// </summary>
 public static class Extensions
 {
     /// <summary>
-    /// Adds a singleton service for the <see cref="StackExchange.Redis.IConnectionMultiplexer"/>
-    /// (<see cref="ConnectionMultiplexer"/>) and a transient service for the
-    /// <see cref="IDistributedCacheService"/> (<see cref="RedisDistributedCacheService"/>).
+    /// Adds Redis distributed caching services to the service collection.
     /// </summary>
     ///
     /// <param name="services">
     /// The service collection to add the services to.
     /// </param>
-    /// <param name="redisConnectionString">
-    /// The connection string for Redis.
-    /// </param>
-    ///
-    /// <returns>
-    /// The service collection with the added services.
-    /// </returns>
-    public static IServiceCollection AddRedisCaching(
-        this IServiceCollection services,
-        string redisConnectionString)
+    extension(IServiceCollection services)
     {
-        services.AddSingleton<IConnectionMultiplexer>(_ =>
-            ConnectionMultiplexer.Connect(redisConnectionString));
-        services.AddTransient<IDistributedCacheService, RedisDistributedCacheService>();
-        return services;
+        /// <summary>
+        /// Adds Redis distributed caching services to the service collection.
+        /// </summary>
+        ///
+        /// <param name="redisConnectionString">
+        /// The Redis connection string.
+        /// </param>
+        ///
+        /// <returns>
+        /// The updated service collection.
+        /// </returns>
+        public IServiceCollection AddRedisCaching(string redisConnectionString)
+        {
+            services.AddSingleton<IConnectionMultiplexer>(_ =>
+                ConnectionMultiplexer.Connect(redisConnectionString));
+
+            // The handlers for distributed cache operations.
+            services.AddTransient(typeof(IDistributedCacheService.IGetHandler<>), typeof(Get<>));
+            services.AddTransient(typeof(IDistributedCacheService.ISetHandler<>), typeof(Set<>));
+            services.AddTransient<IDistributedCacheService.IRemoveHandler, Remove>();
+            services.AddTransient<IDistributedCacheService.IExistsHandler, Exists>();
+
+            return services;
+        }
     }
 }
